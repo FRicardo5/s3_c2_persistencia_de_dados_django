797dee92ef2104601f5d7b72d1905df7c1e272b4
FRicardo5 <felipe.ferreira@bsatech.io>
2025-10-15 17:02:15 -0300

    first commit

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
+
+dados = {
+    1: {"nome":"nebulosa de carinho",
+        "legenda": "webtelecope.org / NASA / James Web"},
+    2: {"nome":"Galáxia NGC 1079",
+        "legenda": "nasa.org / NASA / Hubble"}
+}
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
